32c3c87cd5a7d5046914245bee4faf5483f2deab
Bortniak Volodymyr <vbortniak@VBORTNIAK-MBP.local>
2025-05-24 21:44:40 +0200

    sync file on Create event

--- main.go
+++ main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"github.com/Bortnyak/file-syncer/pkg/client"
 	"github.com/Bortnyak/file-syncer/pkg/server"
 	"github.com/Bortnyak/file-syncer/pkg/watcher"
 )
 
 func main() {
 	go watcher.Watch()
-	server.Main()
+	go server.Main()
+	go client.ListenToUpdates()
+
+	select {}
 }
